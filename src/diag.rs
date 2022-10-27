ccc50445355be6cf0237e604da13e18b0d64040f
Jonathan <Razordor@gmail.com>
2022-10-27 12:25:09 -0700

    refactored to work with dylink crate

--- src/diag.rs
+++ src/diag.rs
@@ -1,18 +1,18 @@
 // This emits warnings for attributes where applicable
+#[cfg(feature = "warn_diag")]
 pub(crate) fn foreign_mod_warn(foreign_mod: &syn::ItemForeignMod) {
-	let doc_ident = syn::parse_str::<syn::Ident>("doc").unwrap();
+    use syn::spanned::Spanned;
     let mut doc_spans = Vec::new();
     for mod_attr in foreign_mod.attrs.iter() {
-        if mod_attr.path.is_ident(&doc_ident) {
-            doc_spans.push(mod_attr.pound_token.spans[0]);
+        if mod_attr.path.is_ident("doc") {
+            doc_spans.push(mod_attr.span());
         }
     }
     if !doc_spans.is_empty() {
         let mut span = doc_spans.remove(0);
-        for item in doc_spans.iter() {
-            span = span.join(*item).unwrap();
+        for item in doc_spans.into_iter() {
+            span = span.join(item).unwrap();
         }
-
         proc_macro::Diagnostic::spanned(
             span.unwrap(),
             proc_macro::Level::Warning,
@@ -22,3 +22,7 @@ pub(crate) fn foreign_mod_warn(foreign_mod: &syn::ItemForeignMod) {
         .emit();
     }
 }
+
+/*pub(crate) fn expect_token(tk: proc_macro2::TokenTree) {
+
+}*/
--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,26 @@ use std::sync::atomic::{AtomicU64, Ordering};
 
 use proc_macro::TokenStream as TokenStream1;
 use proc_macro2::{TokenStream as TokenStream2, *};
+use syn::parse_macro_input;
 
 mod diag;
+
+// TODO: add a derive macro that deals with the dylink vulkan trait
 
 #[proc_macro_attribute]
 pub fn dylink(args: TokenStream1, input: TokenStream1) -> TokenStream1 {
     let args = TokenStream2::from(args);
-    let input = TokenStream2::from(input);
+    let foreign_mod = parse_macro_input!(input as syn::ItemForeignMod);
 
-    let foreign_mod: syn::ItemForeignMod = syn::parse2(input).unwrap();
-
+    #[cfg(feature = "warn_diag")]
     diag::foreign_mod_warn(&foreign_mod);
 
-    let link_type = get_link_type(args).unwrap();
+    let link_type = match get_link_type(syn::parse2(args).unwrap()) {
+        Ok(tk) => tk,
+        Err(e) => {
+            return e.into_compile_error().into();
+        }
+    };
     let mut ret = TokenStream::new();
     for item in foreign_mod.items {
         use syn::ForeignItem;
@@ -32,19 +39,16 @@ pub fn dylink(args: TokenStream1, input: TokenStream1) -> TokenStream1 {
     TokenStream1::from(ret)
 }
 
-fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &str) -> TokenStream2 {
+fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &TokenStream2) -> TokenStream2 {
     let fn_name = fn_item.sig.ident.into_token_stream();
     let abi = abi.into_token_stream();
     let vis = fn_item.vis.into_token_stream();
     let output = fn_item.sig.output.into_token_stream();
-    let link_type: TokenStream2 = link_type.parse().unwrap();
 
     let mut fn_attrs = TokenStream2::new();
     for attr in fn_item.attrs {
         fn_attrs.extend(attr.into_token_stream());
     }
-
-    let lazyfn_path: TokenStream2 = "dylink::lazyfn".parse().unwrap();
 
     let mut params_no_type = TokenStream2::new();
     let mut params_with_type = TokenStream2::new();
@@ -68,12 +72,16 @@ fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &str) -> Tok
     .parse()
     .unwrap();
 
-    let unsafety: TokenStream2 = fn_item.sig.unsafety.map_or(TokenStream2::new(), |r| r.to_token_stream());
+    let unsafety: TokenStream2 = fn_item
+        .sig
+        .unsafety
+        .map_or(TokenStream2::new(), |r| r.to_token_stream());
 
     quote! {
         #[doc(hidden)]
+        #[inline(never)]
         #unsafety #abi fn #initial_fn(#params_with_type) #output {
-            match #fn_name.link_addr(#lazyfn_path::#link_type) {
+            match #fn_name.link_lib(stringify!(#fn_name), dylink::lazyfn::#link_type) {
                 Ok(function) => function(#params_no_type),
                 Err(err) => panic!("{}", err),
             }
@@ -82,49 +90,49 @@ fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &str) -> Tok
         #[allow(non_upper_case_globals)]
         #fn_attrs
         #vis static #fn_name
-        : #lazyfn_path::LazyFn<#unsafety #abi fn(#params_default) #output>
-        = #lazyfn_path::LazyFn::new(stringify!(#fn_name), #initial_fn);
+        : dylink::lazyfn::LazyFn<#unsafety #abi fn(#params_default) #output>
+        = dylink::lazyfn::LazyFn::new(#initial_fn);
     }
 }
 
-fn get_link_type(args: TokenStream2) -> Result<String, LexError> {
-    let mut default_attr = 0;
-    let mut lib_name = String::new();
-    for (i, token) in args.into_iter().enumerate() {
-        match token {
-            TokenTree::Ident(ident) => {
-                assert_eq!(i, 0);
-                match ident.to_string().as_str() {
-                    "name" => default_attr = 1,
-                    name => lib_name = name.to_string(),
-                }
+fn get_link_type(args: syn::Expr) -> Result<TokenStream2, syn::Error> {
+    use std::str::FromStr;
+    use syn::{spanned::Spanned, *};
+    match args {
+        Expr::Path(ExprPath { path, .. }) => {
+            if path.is_ident("vulkan") {
+                Ok(TokenStream2::from_str("LinkType::Vulkan").unwrap())
+            } else if path.is_ident("opengl") {
+                Ok(TokenStream2::from_str("LinkType::OpenGL").unwrap())
+            } else {
+                Err(Error::new(
+                    path.span(),
+                    "expected `vulkan`, `opengl`, or `name`",
+                ))
             }
-            TokenTree::Punct(punct) => {
-                assert_eq!(i, 1);
-                if punct.as_char() == '=' {
-                    default_attr += 1;
-                }
-            }
-            TokenTree::Literal(library) => {
-                assert_eq!(i, 2);
-                default_attr += 1;
-                lib_name = library.to_string();
-                lib_name.pop().unwrap();
-            }
-            _ => panic!(),
         }
+        Expr::Assign(assign) => {
+            if let Expr::Path(ExprPath { path, .. }) = *assign.left {
+                if !path.is_ident("name") {
+                    return Err(Error::new(path.span(), "expected `name`"));
+                }
+            } else {
+                panic!()
+            }
+            if let Expr::Lit(ExprLit { lit, .. }) = *assign.right {
+                if let Lit::Str(lib) = lit {
+                    Ok(
+                        format!("LinkType::General{{library: \"{}.dll\"}}", lib.value())
+                            .parse()
+                            .unwrap(),
+                    )
+                } else {
+                    Err(Error::new(lit.span(), "expected `name`"))
+                }
+            } else {
+                panic!()
+            }
+        }
+        _ => panic!(),
     }
-    assert!(
-        lib_name == "vulkan" || lib_name == "opengl" || default_attr == 3,
-        "invalid attribute parameter"
-    );
-
-    let ret = match lib_name.as_str() {
-        "vulkan" => "LinkType::Vulkan".to_owned(),
-        "opengl" => "LinkType::OpenGL".to_owned(),
-        lib_name => {
-            format!("LinkType::General{{library: {lib_name}.dll\"}}")
-        }
-    };
-    Ok(ret)
 }
