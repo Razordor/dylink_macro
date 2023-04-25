cf4196063cf4a7c50043886f4c2b63d008f1fd52
Jonathan <Razordor@gmail.com>
2023-04-24 18:25:54 -0700

    updated internal parameter variables

--- src/lib.rs
+++ src/lib.rs
@@ -139,7 +139,9 @@ fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &LinkType) -
             }
             static DYN_FUNC
             : dylink::LazyFn<#abi fn (#params_default) #output>
-            = dylink::LazyFn::new(initial_fn);
+            = dylink::LazyFn::new(
+                std::sync::atomic::AtomicPtr::new(unsafe {std::mem::transmute(&(initial_fn as #abi fn (#params_default) #output))})
+            );
 
             #call_dyn_func
         }
