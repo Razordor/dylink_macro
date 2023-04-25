f0b2de61d31dbf3143d972bcb2d935b54c149a1f
Jonathan <Razordor@gmail.com>
2023-04-24 19:30:07 -0700

    made static reference explicit

--- src/lib.rs
+++ src/lib.rs
@@ -137,10 +137,11 @@ fn parse_fn(abi: &syn::Abi, fn_item: syn::ForeignItemFn, link_type: &LinkType) -
                     Err(err) => panic!("{}", err),
                 }
             }
+            static DYN_FUNC_REF: &'static #abi fn (#params_default) #output = &(initial_fn as #abi fn (#params_default) #output);
             static DYN_FUNC
             : dylink::LazyFn<#abi fn (#params_default) #output>
             = dylink::LazyFn::new(
-                std::sync::atomic::AtomicPtr::new(unsafe {std::mem::transmute(&(initial_fn as #abi fn (#params_default) #output))})
+                std::sync::atomic::AtomicPtr::new(unsafe {std::mem::transmute(DYN_FUNC_REF)})
             );
 
             #call_dyn_func
